1f7967d6ea816e0206d18c4bb2e49c4622fb6de7
Rui <andalusiasara@gmail.com>
2024-09-28 13:31:44 +0800

    Add hero-text.tsx

---
+++ app/ui/hero-text.tsx
@@ -0,0 +1,8 @@
+export default function HeroText() {
+  return (
+    <div className="flex flex-col text-center !mb-10 space-y-5">
+      <h1 className="text-5xl font-[family-name:var(--font-tiemposLight-serif)] font-thin mb-3">Rotate PDF Pages</h1>
+      <p className="mt-2 text-gray-600 max-w-lg mx-auto tracking-normal">Simply click on a page to rotate it. You can then download your modified PDF.</p>
+    </div>
+  );
+}
