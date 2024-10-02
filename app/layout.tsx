792758fb11e2670abe5d6789c8c92de95f0347a9
Rui <andalusiasara@gmail.com>
2024-10-02 14:26:21 +0800

    Update favicon.ico and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,6 @@ import localFont from "next/font/local";
 import { inter } from "@/app/ui/fonts";
 import "@/app/ui/globals.css";
 
-const tiemposLightSerif =  localFont({
-  src: "./fonts/TiemposLight.woff",
-  variable: "--font-tiemposLight-serif",
-  weight: "500",
-});
-
-const tiemposHeadlineSerif =  localFont({
-  src: "./fonts/TiemposHeadline.woff",
-  variable: "--font-tiemposHeadline-serif",
-  weight: "500",
-});
-
 export const metadata: Metadata = {
   title: "Free PDF Page Rotator - Rotate Individual or All Pages",
   description: "Rotate individual or all pages in your PDF effortlessly. No downloads or sign-ups. Fast, secure, and user-friendly. Try now!",
@@ -46,6 +34,18 @@ export const metadata: Metadata = {
     type: 'website',
   },
 };
+
+const tiemposLightSerif =  localFont({
+  src: "./fonts/TiemposLight.woff",
+  variable: "--font-tiemposLight-serif",
+  weight: "500",
+});
+
+const tiemposHeadlineSerif =  localFont({
+  src: "./fonts/TiemposHeadline.woff",
+  variable: "--font-tiemposHeadline-serif",
+  weight: "500",
+});
 
 export default function RootLayout({
   children,
--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -2,7 +2,8 @@ export default function Footer() {
   return (
     <footer className="bg-white" aria-labelledby="footer-heading">
       <h2 id="footer-heading" className="sr-only">Footer</h2>
-      <div className="mx-auto max-w-7xl px-6 pb-8 mt-8 sm:mt-12 lg:px-8 lg:mt-16 border-t border-gray-900/10 pt-16">
+      <div className="mx-auto max-w-7xl px-6 pb-8 mt-8 sm:mt-12
+      md:px-8 lg:px-16 lg:mt-16 border-t border-gray-900/10 pt-16">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-8">
             <img className="h-7" src="/favicon.ico" alt="PDF.ai logo"/>
