9c1dc854431bf6a63f2f5019dfc51fd7e29a806e
Rui <andalusiasara@gmail.com>
2024-10-02 13:32:37 +0800

    Update metadata for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,35 @@ const tiemposHeadlineSerif =  localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Free PDF Page Rotator = Rotate Individual or All Pages",
+  title: "Free PDF Page Rotator - Rotate Individual or All Pages",
   description: "Rotate individual or all pages in your PDF effortlessly. No downloads or sign-ups. Fast, secure, and user-friendly. Try now!",
+  metadataBase: new URL('https://pdf.ai'),
+  icons: {
+    icon: '/favicon.ico',
+  },
+  alternates: {
+    canonical: '/',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@PDFdotai',
+  },
+  openGraph: {
+    title: 'Free PDF Page Rotator - Rotate Individual or All Pages',
+    description: 'Rotate individual or all pages in your PDF effortlessly. No downloads or sign-ups. Fast, secure, and user-friendly. Try now!',
+    url: 'https://pdf.ai',
+    images: [
+      {
+        url: 'https://imagedelivery.net/pcavElAZUUevXK53Dl4vWA/4738d269-b536-45f2-57e1-fe07fef90d00/public',
+        type: 'image/jpeg',
+      },
+    ], 
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
