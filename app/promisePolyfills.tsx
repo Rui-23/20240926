ba1cbb90d0784bb100cebecd2b6de57627b04d11
Rui <andalusiasara@gmail.com>
2024-10-03 15:27:39 +0800

    Replace any with unknown in promisePolyfills.tsx

--- app/promisePolyfills.tsx
+++ app/promisePolyfills.tsx
@@ -1,14 +1,14 @@
 export type PromiseWithResolvers<T> = {
   promise: Promise<T>;
   resolve: (value: T | PromiseLike<T>) => void;
-  reject: (reason?: any) => void;
+  reject: (reason?: unknown) => void;
 };
 
 export function polyfillPromiseWithResolvers() {
   if (!Promise.withResolvers) {
     Promise.withResolvers = function <T>(): PromiseWithResolvers<T> {
       let resolve: (value: T | PromiseLike<T>) => void;
-      let reject: (reason?: any) => void;
+      let reject: (reason?: unknown) => void;
 
       const promise = new Promise<T>((res, rej) => {
         resolve = res;
