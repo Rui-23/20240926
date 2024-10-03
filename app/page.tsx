fc2f948214a7bace20816ad275620f0680027327
Rui <andalusiasara@gmail.com>
2024-10-03 15:21:23 +0800

    Update polyfillPromiseWithResolvers

--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,33 @@ import { inter } from "@/app/ui/fonts";
 import Header from '@/app/ui/header';
 import Footer from '@/app/ui/footer';
 import Body from "@/app/ui/body";
+import { polyfillPromiseWithResolvers } from "@/app/promisePolyfills";
 
-if (typeof Promise.withResolvers === "undefined") {
-  if (window) {
-    // @ts-expect-error This does not exist outside of polyfill which this is doing
-    window.Promise.withResolvers = function () {
-      let resolve, reject
-      const promise = new Promise((res, rej) => {
-        resolve = res
-        reject = rej
-      })
-      return { promise, resolve, reject }
-    }
-  } else {
-    // @ts-expect-error This does not exist outside of polyfill which this is doing
-    global.Promise.withResolvers = function () {
-      let resolve, reject
-      const promise = new Promise((res, rej) => {
-        resolve = res
-        reject = rej
-      })
-      return { promise, resolve, reject }
-    }
-  }
-}
+polyfillPromiseWithResolvers();
+
+// if (typeof Promise.withResolvers === "undefined") {
+//   if (window) {
+//     // @ts-expect-error This does not exist outside of polyfill which this is doing
+//     window.Promise.withResolvers = function () {
+//       let resolve, reject
+//       const promise = new Promise((res, rej) => {
+//         resolve = res
+//         reject = rej
+//       })
+//       return { promise, resolve, reject }
+//     }
+//   } else {
+//     // @ts-expect-error This does not exist outside of polyfill which this is doing
+//     global.Promise.withResolvers = function () {
+//       let resolve, reject
+//       const promise = new Promise((res, rej) => {
+//         resolve = res
+//         reject = rej
+//       })
+//       return { promise, resolve, reject }
+//     }
+//   }
+// }
 
 export default function Home() {
   return (
---
+++ app/promisePolyfills.tsx
@@ -0,0 +1,48 @@
+export type PromiseWithResolvers<T> = {
+  promise: Promise<T>;
+  resolve: (value: T | PromiseLike<T>) => void;
+  reject: (reason?: any) => void;
+};
+
+export function polyfillPromiseWithResolvers() {
+  if (!Promise.withResolvers) {
+    Promise.withResolvers = function <T>(): PromiseWithResolvers<T> {
+      let resolve: (value: T | PromiseLike<T>) => void;
+      let reject: (reason?: any) => void;
+
+      const promise = new Promise<T>((res, rej) => {
+        resolve = res;
+        reject = rej;
+      });
+
+      return { promise, resolve: resolve!, reject: reject! };
+    };
+  }
+}
+
+
+// if (typeof Promise.withResolvers === "undefined") {
+//     if (window) {
+//       // @ts-expect-error This does not exist outside of polyfill which this is doing
+//       window.Promise.withResolvers = function () {
+//         let resolve, reject
+//         const promise = new Promise((res, rej) => {
+//           resolve = res
+//           reject = rej
+//         })
+//         return { promise, resolve, reject }
+//       }
+//     } else {
+//       // @ts-expect-error This does not exist outside of polyfill which this is doing
+//       global.Promise.withResolvers = function () {
+//         let resolve, reject
+//         const promise = new Promise((res, rej) => {
+//           resolve = res
+//           reject = rej
+//         })
+//         return { promise, resolve, reject }
+//       }
+//     }
+//   }
+
+//export default Promise;
